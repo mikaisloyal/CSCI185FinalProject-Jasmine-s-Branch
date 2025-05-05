a8a8e6a0760a2bd820d560f59f9d2b0a4c772794
TRGGB2 <sopfrieaitako@gmail.com>
2025-05-04 22:17:06 -0400

    Initalization of project

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+/*
+ProjectName
+CSCI 185
+M03
+Aron Lin, Rita Chen, Htoo Naing, Zhasmin Tuiachieva
+Date of last contribution (Use the date on Github commit)
+*/
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Hello World");
+
+    }
+}
+
+
+
+
