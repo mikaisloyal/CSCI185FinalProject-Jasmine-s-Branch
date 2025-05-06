a502e215a85fc767dec099bed9a01d07c48c5384
RXCHEN1 <chenrita51@gmail.com>
2025-05-06 13:24:35 -0400

    Created a university class, a custom university/location dropdown panel, and basic testing in the main class

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ M03
 Aron Lin, Rita Chen, Htoo Naing, Zhasmin Tuiachieva
 Date of last contribution (Use the date on Github commit)
 */
-
+package src;
 import java.awt.*;
 import javax.swing.*;
 
@@ -21,12 +21,20 @@ class Main {
         JPanel mainPanel = new JPanel();
         LayoutManager layout1 = new GridLayout(5,5,10,10);
         mainPanel.setLayout(layout1);
-        
+
+        //Creating custom panel from UniversityDropDownPanel class
+        UniversityDropdownPanel uniLocationPanel = new UniversityDropdownPanel();
+
+        //Components
         JLabel title = new JLabel("Name Eventually");
         title.setHorizontalAlignment(0);
 
+        //Adding components to panel
         mainPanel.add(title);
-        
+
+        //Adding panels to frame
+        mainFrame.add(uniLocationPanel);
+
 
         mainFrame.setVisible(true);
     }
